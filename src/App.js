a975dc2fab1cc684f03461958862aaeb68c08e6f
ANURAG SHARMA <anuurag2001@gmail.com>
2022-03-19 19:50:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import MovieCard from "./MovieCard";
 
 const API_URL =  "http://www.omdbapi.com?apikey=31787861";
 
-const movie1 = {
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
-    "Title": "Italian Spiderman",
-    "Type": "movie",
-    "Year": "2007",
-    "imdbID": "tt2705436"
-}
-
 const App = () => {
 
     const [movies,setMovies] = useState([]);
